a13b92fc19ea47e874fa258a035950c6a059ae1e
prabesht80 <prabesht80@gmail.com>
2020-10-22 15:57:33 +0545

    Updates

---
+++ precache-manifest.96d8c218207840926e374e1c67aad84f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311cfcb6f97a0a69ff35a43f328d396d",
+    "url": "/hulu-Clone/index.html"
+  },
+  {
+    "revision": "d5bf4163bb12536cae85",
+    "url": "/hulu-Clone/static/css/main.dc8f149e.chunk.css"
+  },
+  {
+    "revision": "9701f819468221368a75",
+    "url": "/hulu-Clone/static/js/2.7034d574.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hulu-Clone/static/js/2.7034d574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5bf4163bb12536cae85",
+    "url": "/hulu-Clone/static/js/main.c6105de6.chunk.js"
+  },
+  {
+    "revision": "8f40e96e68cbb98d00d7",
+    "url": "/hulu-Clone/static/js/runtime-main.dc773135.js"
+  }
+]);
